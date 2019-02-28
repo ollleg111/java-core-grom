836134ef945c626411512b8b16655c4813d24967
oleg.maliy14@gmail.com <q2301299266>
2019-02-28 21:21:44 +0200

    28.02.2019 -1-

--- src/lesson28/hw/DateComparator.java
+++ src/lesson28/hw/DateComparator.java
@@ -6,8 +6,11 @@ public class DateComparator implements Comparator<Capability> {
 
     @Override
     public int compare(Capability o1, Capability o2) {
-        if (!o1.getDateCreated().equals(o2.getDateCreated()))
+//        if (!o1.getDateCreated().equals(o2.getDateCreated()))
+//            return o1.getDateCreated().compareTo(o2.getDateCreated());
+//        else return 0; 
+
             return o1.getDateCreated().compareTo(o2.getDateCreated());
-        else return 0;
+
     }
 }
