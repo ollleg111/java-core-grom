3d7082aabd4ff8f3a471a7dce83f0e4916bb7683
oleg.maliy14@gmail.com <q2301299266>
2019-03-03 15:21:34 +0200

    03.03.2019 -0-

--- src/lesson29/hw/HashSetTest.java
+++ src/lesson29/hw/HashSetTest.java
@@ -1,22 +1,24 @@
 package lesson29.hw;
 
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Iterator;
 
 public class HashSetTest {
     public static void main(String[] args) {
 
-        System.out.println(useHashSet());
+        System.out.println(useHashSet().toString());
     }
 
-    public static HashSetTest useHashSet() {
+    public static HashSet useHashSet() {
         HashSet<Order> list = new HashSet<>();
 
-        Order order0 = new Order(100, 100, "UAH", "aaa", "a0");
+        Order order0 = new Order(100, 2700, "UAH", "aaa", "a0");
         Order order1 = new Order(101, 100, "USD", "bbb", "a1");
-        Order order2 = new Order(102, 100, "GBP", "ccc", "a2");
-        Order order3 = new Order(103, 100, "CAD", "ddd", "a3");
-        Order order4 = new Order(104, 100, "RUR", "eee", "a4");
-
+        Order order2 = new Order(102, 80, "GBP", "ccc", "a2");
+        Order order3 = new Order(103, 130, "CAD", "ddd", "a3");
+        Order order4 = new Order(104, 4000, "RUR", "eee", "a4");
+        Order order5 = new Order(105, 4000000, "ZBD", "fff", "z0");
 
         list.add(order0);
         list.add(order1);
@@ -26,17 +28,59 @@ public class HashSetTest {
 
         System.out.println(list.toString());
 
+        for (Order order : list) {
+            System.out.println(order.toString());
+        }
 
         list.remove(order0);
         System.out.println(list.toString());
 
+        System.out.println("________________________________________remove___________________________________");
+
+        for (Order order : list) {
+            System.out.println(order.toString());
+        }
+
+        Object[] object = list.toArray();
+        System.out.println("!!  " + Arrays.toString(object));
+        System.out.println("________________________________________toArray__________________________________");
+
+
+
+        System.out.println(list.size() + " elements in HashSetList");
+
+        System.out.println("________________________________________size_____________________________________");
 
         list.add(order0);
         list.add(order1);
         list.add(order2);
         list.add(order3);
         list.add(order4);
+//
+//        for (Order order : list) {
+//            System.out.println(order.toString());
+//        }
 
-        return null;
+        HashSet<Order> retainAllList = new HashSet<>();
+
+        retainAllList.add(order2);
+        retainAllList.add(order3);
+        retainAllList.add(order5);
+
+        list.retainAll(retainAllList);
+
+        Iterator<Order> orderIterator = list.iterator();
+
+        while (orderIterator.hasNext()) {
+            System.out.println(orderIterator.next().toString());
+        }
+
+        System.out.println("________________________________________retainAll________________________________");
+
+        list.add(order0);
+        list.add(order1);
+        list.add(order4);
+
+        return list;
     }
 }
--- src/lesson29/hw/Order.java
+++ src/lesson29/hw/Order.java
@@ -16,6 +16,30 @@ public class Order {
         this.shopIdentificator = shopIdentificator;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Order order = (Order) o;
+
+        if (id != order.id) return false;
+        if (price != order.price) return false;
+        if (!currency.equals(order.currency)) return false;
+        if (!itemName.equals(order.itemName)) return false;
+        return shopIdentificator.equals(order.shopIdentificator);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + price;
+        result = 31 * result + currency.hashCode();
+        result = 31 * result + itemName.hashCode();
+        result = 31 * result + shopIdentificator.hashCode();
+        return result;
+    }
+
     public long getId() {
         return id;
     }
