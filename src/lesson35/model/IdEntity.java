78489a2ee6d540277d255ba525c6cd02af0044ff
oleg.maliy14@gmail.com <q2301299266>
2019-05-31 23:47:31 +0300

    30.05.2019 -2-

--- src/lesson35/model/IdEntity.java
+++ src/lesson35/model/IdEntity.java
@@ -3,6 +3,4 @@ package lesson35.model;
 public abstract class IdEntity {
     public abstract long getId();
     public abstract void setId(long id);
-    public abstract boolean equals(Object o);
-    public abstract int hashCode();
 }
